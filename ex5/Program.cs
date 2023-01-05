fba6e67d8a3d7b76aead1022f936e158e123b157
Nastya_krevtka <nastyamkala1278@gmail.com>
2023-01-05 12:29:58 +0300

    задача5 спираль

---
+++ ex5/Program.cs
@@ -0,0 +1,36 @@
+﻿//Напишите программу, которая заполнит спирально массив 4 на 4.
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write(matr[i, j]);
+            Console.Write(" ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int n = 4;
+int[,] matrix = new int[n, n];
+
+int temp = 1;
+int i = 0;
+int j = 0;
+
+while (temp <= matrix.GetLength(0) * matrix.GetLength(1))
+{
+  matrix[i, j] = temp;
+  temp++;
+  if (i <= j + 1 && i + j < matrix.GetLength(1) - 1)
+    j++;
+  else if (i < j && i + j >= matrix.GetLength(0) - 1)
+    i++;
+  else if (i >= j && i + j > matrix.GetLength(1) - 1)
+    j--;
+  else
+    i--;
+}
+
+PrintArray(matrix);
